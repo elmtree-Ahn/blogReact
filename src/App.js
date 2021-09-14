319ea1d72433fdea2f9fba8e06c7a2495ada3f83
elmtree-Ahn <clfclf3138@gmail.com>
2021-09-14 22:21:23 +0900

    useState로 state를 만들고 변경함수도 사용해보기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,53 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
 
-  let posts = "강남 고기 맛집";
+  let [title, setTitle] = useState(['리액트 시작하기', '대패삼겹살 끝내주게 먹는 법', '미사호수공원 라이딩']);
+  let [thumbsUp, setThumbsUp] = useState(0);
+
+  function changeTitle() {
+    let newArray = [...title];
+    newArray[0] = '뷰 시작하기';
+    setTitle( newArray );
+  }
+
+  function sortTitle() {
+    let newArray = [...title];
+    newArray = newArray.sort()
+    setTitle( newArray )    
+  }
 
   return (
     <div className="App">
+      {/* 헤더 */}
       <div className="black-nav">
         <div>개발해블로그</div>
       </div>
-      <h4>{ posts }</h4>
+      {/* 포스트 리스트 */}
+      <button onClick={ changeTitle }>언어 바꾸기</button>
+      <button onClick={sortTitle}>ㄱㄴㄷ 순으로</button>
+
+      <div className="list">
+        <h4>
+          { title[0] }
+          <span onClick={ ()=>{ setThumbsUp(thumbsUp + 1)} }>👍</span> { thumbsUp }
+        </h4>
+        <p>9월 14일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h4>{ title[1] }</h4>
+        <p>9월 10일 발행</p>
+        <hr />
+      </div>
+      <div className="list">
+        <h4>{ title[2] }</h4>
+        <p>9월 02일 발행</p>
+        <hr />
+      </div>
+      
 
     </div>
   );
