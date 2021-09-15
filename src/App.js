4974608a2747d29622378e85a1f8e235575b796d
elmtree-Ahn <clfclf3138@gmail.com>
2021-09-15 12:16:39 +0900

    반복문 사용과 component로 UI 꾸미기

--- src/App.js
+++ src/App.js
@@ -7,17 +7,7 @@ function App() {
   let [title, setTitle] = useState(['리액트 시작하기', '대패삼겹살 끝내주게 먹는 법', '미사호수공원 라이딩']);
   let [thumbsUp, setThumbsUp] = useState(0);
 
-  function changeTitle() {
-    let newArray = [...title];
-    newArray[0] = '뷰 시작하기';
-    setTitle( newArray );
-  }
-
-  function sortTitle() {
-    let newArray = [...title];
-    newArray = newArray.sort()
-    setTitle( newArray )    
-  }
+  // let [modal, setModal] = useState(false);
 
   return (
     <div className="App">
@@ -26,9 +16,6 @@ function App() {
         <div>개발해블로그</div>
       </div>
       {/* 포스트 리스트 */}
-      <button onClick={ changeTitle }>언어 바꾸기</button>
-      <button onClick={sortTitle}>ㄱㄴㄷ 순으로</button>
-
       <div className="list">
         <h4>
           { title[0] }
@@ -48,9 +35,42 @@ function App() {
         <hr />
       </div>
       
+      {
+        title.map((title) => {
+          return (
+            <div className="list">
+              <h4>
+                { title }
+                <span onClick={ ()=>{ setThumbsUp(thumbsUp + 1)} }>👍</span> { thumbsUp }
+              </h4>
+              <p>9월 14일 발행</p>
+              <hr />
+            </div>
+          )
+        })
+      }
+      
+
+    {/* <button onClick={ () => {setModal(!modal)} }>모달</button>
+
+    {
+      modal === true
+      ? <Modal />
+      : null
+    } */}
 
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
